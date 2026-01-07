e8cf363f98e89655ca699b8d86b7cfa2b79864ec
touguy <touguy@korea.com>
2026-01-07 15:06:45 +0900

    KafkaContext 사용

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -1,17 +1,16 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { MessagePattern, Payload, Ctx, KafkaContext } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   @MessagePattern('math.sum') // 토픽명 'math.sum' 구독
-  accumulate(@Payload() message: any): number {
-    // 로그를 찍어 실제 구조를 확인해보세요. 
-    // 보통 NestJS는 message.value에 데이터를 담습니다.
-    console.log('실제 수신 데이터 전체:', message);
-    // 카프카 메시지는 { value: ... } 형태로 들어옵니다.
-    // 데이터 추출 (직접 접근 또는 구조 분해)
-    const data = message.value ? message.value : message;
-    console.log('카프카로부터 받은 데이터:', data);
-    return (data || []).reduce((a, b) => a + b, 0);
+  accumulate(@Payload() data: number[], @Ctx() context: KafkaContext): number {
+    const originalMessage = context.getMessage(); // 카프카의 원본 메시지 전체 접근
+    const partition = context.getPartition();    // 파티션 번호 확인
+
+    console.log('원본 메시지:', originalMessage);
+    console.log('파티션 번호:', partition);
+
+    return data.reduce((a, b) => a + b, 0);
   }
 }
